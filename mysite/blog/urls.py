4e12c38a2e9f6a5245eff3520880cedc1e5d972c
Stupel <marian.holly7117@gmail.com>
2024-11-26 15:21:05 +0100

    creating SEO-friendly URLs for posts

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -5,5 +5,5 @@ app_name = 'bloggy'
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('<int:id>/', views.post_detail, name='post_detail'),
+    path('<int:id>/<int:month>/<int:year>/<slug:post>/', views.post_detail, name='post_detail'),
 ] 
