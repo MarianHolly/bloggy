d821e50ce0cd24e3a2ccb180cd8c1902d1df3ab3
Stupel <marian.holly7117@gmail.com>
2024-11-30 14:49:22 +0100

    similar posts

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,16 +1,24 @@
 from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Count
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 
+from taggit.models import Tag
+
 from .forms import CommentForm, EmailPostForm
 from .models import Post
 
-
-def post_list(request):
+def post_list(request, tag_slug=None):
     # QuerySet
     post_list = Post.published.all()
+
+    # Tags
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
 
     # Pagination
     paginator = Paginator(post_list, 3)
@@ -25,7 +33,7 @@ def post_list(request):
         posts = paginator.page(paginator.num_pages)
 
     # Rendering
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    return render(request, 'blog/post/list.html', {'posts': posts, 'tag': tag})
 
 def post_detail(request, year, month, day, post):
     # QuerySet
@@ -41,11 +49,21 @@ def post_detail(request, year, month, day, post):
     # Form for users to comment
     form = CommentForm()
 
+    # List of Simiralar Posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(
+        tags__in=post_tags_ids
+    ).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(
+        same_tags=Count('tags')
+    ).order_by('-same_tags', '-publish')[:4]
+
+
     # Rendering
     return render(
         request, 
         'blog/post/detail.html', 
-        {'post': post, 'comments': comments, 'form': form})
+        {'post': post, 'comments': comments, 'form': form, 'similar_posts': similar_posts})
 
 
 # Class-based views to list posts
